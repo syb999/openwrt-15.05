d5623b638e3022555418fcd1306d850524d035e2
syb999 <13062678135@163.com>
2019-10-24 23:04:21 +0800

    Remove luci-app-wifi-disconnect-low-signal
    In this version of hostapd, luci-app-wifi-disconnect-low-signal is no longer needed.

--- package/luci-app-wifi-disconnect-low-signal/files/usr/lib/lua/luci/controller/wifi-disconnect.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.wifi-disconnect", package.seeall)
-
-function index()
-	
-	entry({"admin", "system", "wifi-disconnect"}, template("wifi-disconnect"), _("wifi-disconnect-low-signal"), 10).leaf = true
-end
