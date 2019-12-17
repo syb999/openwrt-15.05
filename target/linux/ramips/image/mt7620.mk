d11fa494f2ed8b50b436d9e03395c9b1029e5e12
syb999 <13062678135@163.com>
2019-12-17 22:42:15 +0800

    ramips: merge mt7620-8m to mt7620

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -20,6 +20,48 @@ define Build/zyimage
 	$(STAGING_DIR_HOST)/bin/zyimage $(1) $@
 endef
 
+define Device/ai-br100
+  DTS := AIBR100
+  DEVICE_TITLE := Aigale Ai-BR100
+  DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci
+endef
+TARGET_DEVICES += ai-br100
+
+define Device/e1700
+  DTS := E1700
+  IMAGES += factory.bin
+  DEVICE_TITLE := Linksys E1700
+endef
+TARGET_DEVICES += e1700
+
+define Device/mt7620a
+  DTS := MT7620a
+  DEVICE_TITLE := MediaTek MT7620a EVB
+endef
+TARGET_DEVICES += mt7620a
+
+define Device/rt-n14u
+  DTS := RT-N14U
+  IMAGE_SIZE := $(ralink_default_fw_size_8M)
+  DEVICE_TITLE := Asus RT-N14u
+endef
+TARGET_DEVICES += rt-n14u
+
+define Device/psg1208
+  DTS := PSG1208
+  IMAGE_SIZE := $(ralink_default_fw_size_8M)
+  DEVICE_TITLE := Phicomm PSG1208
+endef
+TARGET_DEVICES += psg1208
+
+define Device/psg1218
+  DTS := PSG1218
+  IMAGE_SIZE := $(ralink_default_fw_size_8M)
+  DEVICE_TITLE := Phicomm PSG1218
+endef
+TARGET_DEVICES += psg1218
+
+
 define Device/y1
   DTS := Y1
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
