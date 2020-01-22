d035d9e30b733a5990f5f44e7d33d8574f663661
syb999 <13062678135@163.com>
2020-01-22 18:08:58 +0800

    mac80211: remove invalid code

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -139,11 +139,6 @@ config wifi-iface
 	option encryption none
 	option disassoc_low_ack 0
 	option isolate 0
-	option signal_connect -95
-	option signal_stay -100
-	option signal_strikes 3
-	option signal_poll_time 5
-	option signal_drop_reason 3
 
 EOF
 	devidx=$(($devidx + 1))
