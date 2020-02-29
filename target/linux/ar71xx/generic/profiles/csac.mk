57b04b01733f0e44fd148f9492c7c256e7fe8182
syb999 <13062678135@163.com>
2020-02-29 15:34:17 +0800

    ar71xx: fix CSAC unable to start

---
+++ target/linux/ar71xx/generic/profiles/csac.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/CSAC
+        NAME:=CSAC
+        PACKAGES:=kmod-ath10k ath10k-firmware-qca9888-ct
+endef
+
+define Profile/CSAC/Description
+        Package set optimized for the CSAC board.
+endef
+
+$(eval $(call Profile,CSAC))
