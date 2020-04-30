e77f16564eb700b3707664466eed7f6c0db637f9
syb999 <13062678135@163.com>
2020-04-30 17:13:04 +0800

    ramips: fix typo

--- target/linux/ramips/mt7621-nand/target.mk
+++ target/linux/ramips/mt7621-nand/target.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009 OpenWrt.org
 #
 
-SUBTARGET:=mt7621
-BOARDNAME:=MT7621 based boards
+SUBTARGET:=mt7621-nand
+BOARDNAME:=MT7621 NAND flash based boards
 ARCH_PACKAGES:=ramips_1004kc
 FEATURES+=usb squashfs nand ubifs ramdisk
 CPU_TYPE:=1004kc
@@ -13,5 +13,5 @@ CFLAGS:=-Os -pipe -mmt -mips32r2 -mtune=1004kc
 DEFAULT_PACKAGES += 
 
 define Target/Description
-	Build firmware images for Ralink MT7621 based boards.
+	Build firmware images for Ralink MT7621 with nand flash based boards.
 endef
