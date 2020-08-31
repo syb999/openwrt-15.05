1bb32ea042e6cae6e82e67eb889d56703de8d7d4
syb999 <13062678135@163.com>
2020-08-31 12:50:27 +0800

    px5g: Update

--- package/utils/px5g/px5g.c
+++ package/utils/px5g/px5g.c
@@ -2,7 +2,7 @@
  * px5g - Embedded x509 key and certificate generator based on PolarSSL
  *
  *   Copyright (C) 2009 Steven Barth <steven@midlink.org>
- *   Copyright (C) 2014 Felix Fietkau <nbd@openwrt.org>
+ *   Copyright (C) 2014 Felix Fietkau <nbd@nbd.name>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -30,9 +30,11 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
-#include <polarssl/bignum.h>
-#include <polarssl/x509_crt.h>
-#include <polarssl/rsa.h>
+#include <mbedtls/bignum.h>
+#include <mbedtls/x509_crt.h>
+#include <mbedtls/ecp.h>
+#include <mbedtls/rsa.h>
+#include <mbedtls/pk.h>
 
 #define PX5G_VERSION "0.2"
 #define PX5G_COPY "Copyright (c) 2009 Steven Barth <steven@midlink.org>"
@@ -72,15 +74,32 @@ static void write_file(const char *path, int len, bool pem)
 	fclose(f);
 }
 
-static void write_key(pk_context *key, const char *path, bool pem)
+static mbedtls_ecp_group_id ecp_curve(const char *name)
+{
+	const mbedtls_ecp_curve_info *curve_info;
+
+	if (!strcmp(name, "P-256"))
+		return MBEDTLS_ECP_DP_SECP256R1;
+	else if (!strcmp(name, "P-384"))
+		return MBEDTLS_ECP_DP_SECP384R1;
+	else if (!strcmp(name, "P-521"))
+		return MBEDTLS_ECP_DP_SECP521R1;
+	curve_info = mbedtls_ecp_curve_info_from_name(name);
+	if (curve_info == NULL)
+		return MBEDTLS_ECP_DP_NONE;
+	else
+		return curve_info->grp_id;
+}
+
+static void write_key(mbedtls_pk_context *key, const char *path, bool pem)
 {
 	int len = 0;
 
 	if (pem) {
-		if (pk_write_key_pem(key, (void *) buf, sizeof(buf)) == 0)
+		if (mbedtls_pk_write_key_pem(key, (void *) buf, sizeof(buf)) == 0)
 			len = strlen(buf);
 	} else {
-		len = pk_write_key_der(key, (void *) buf, sizeof(buf));
+		len = mbedtls_pk_write_key_der(key, (void *) buf, sizeof(buf));
 		if (len < 0)
 			len = 0;
 	}
@@ -88,24 +107,33 @@ static void write_key(pk_context *key, const char *path, bool pem)
 	write_file(path, len, pem);
 }
 
-static void gen_key(pk_context *key, int ksize, int exp, bool pem)
+static void gen_key(mbedtls_pk_context *key, bool rsa, int ksize, int exp,
+		    mbedtls_ecp_group_id curve, bool pem)
 {
-	pk_init(key);
-	pk_init_ctx(key, pk_info_from_type(POLARSSL_PK_RSA));
-	fprintf(stderr, "Generating RSA private key, %i bit long modulus\n", ksize);
-	if (rsa_gen_key(pk_rsa(*key), _urandom, NULL, ksize, exp)) {
-		fprintf(stderr, "error: key generation failed\n");
-		exit(1);
+	mbedtls_pk_init(key);
+	if (rsa) {
+		fprintf(stderr, "Generating RSA private key, %i bit long modulus\n", ksize);
+		mbedtls_pk_setup(key, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA));
+		if (!mbedtls_rsa_gen_key(mbedtls_pk_rsa(*key), _urandom, NULL, ksize, exp))
+			return;
+	} else {
+		fprintf(stderr, "Generating EC private key\n");
+		mbedtls_pk_setup(key, mbedtls_pk_info_from_type(MBEDTLS_PK_ECKEY));
+		if (!mbedtls_ecp_gen_key(curve, mbedtls_pk_ec(*key), _urandom, NULL))
+			return;
 	}
+	fprintf(stderr, "error: key generation failed\n");
+	exit(1);
 }
 
-int rsakey(char **arg)
+int dokey(bool rsa, char **arg)
 {
-	pk_context key;
+	mbedtls_pk_context key;
 	unsigned int ksize = 512;
 	int exp = 65537;
 	char *path = NULL;
 	bool pem = true;
+	mbedtls_ecp_group_id curve = MBEDTLS_ECP_DP_SECP256R1;
 
 	while (*arg && **arg == '-') {
 		if (!strcmp(*arg, "-out") && arg[1]) {
@@ -119,22 +147,29 @@ int rsakey(char **arg)
 		arg++;
 	}
 
-	if (*arg)
+	if (*arg && rsa) {
 		ksize = (unsigned int)atoi(*arg);
+	} else if (*arg) {
+		curve = ecp_curve((const char *)*arg);
+		if (curve == MBEDTLS_ECP_DP_NONE) {
+			fprintf(stderr, "error: invalid curve name: %s\n", *arg);
+			return 1;
+		}
+	}
 
-	gen_key(&key, ksize, exp, pem);
+	gen_key(&key, rsa, ksize, exp, curve, pem);
 	write_key(&key, path, pem);
 
-	pk_free(&key);
+	mbedtls_pk_free(&key);
 
 	return 0;
 }
 
 int selfsigned(char **arg)
 {
-	pk_context key;
-	x509write_cert cert;
-	mpi serial;
+	mbedtls_pk_context key;
+	mbedtls_x509write_cert cert;
+	mbedtls_mpi serial;
 
 	char *subject = "";
 	unsigned int ksize = 512;
@@ -145,19 +180,36 @@ int selfsigned(char **arg)
 	time_t from = time(NULL), to;
 	char fstr[20], tstr[20], sstr[17];
 	int len;
+	bool rsa = true;
+	mbedtls_ecp_group_id curve = MBEDTLS_ECP_DP_SECP256R1;
 
 	while (*arg && **arg == '-') {
 		if (!strcmp(*arg, "-der")) {
 			pem = false;
 		} else if (!strcmp(*arg, "-newkey") && arg[1]) {
-			if (strncmp(arg[1], "rsa:", 4)) {
-				fprintf(stderr, "error: invalid algorithm");
+			if (!strncmp(arg[1], "rsa:", 4)) {
+				rsa = true;
+				ksize = (unsigned int)atoi(arg[1] + 4);
+			} else if (!strcmp(arg[1], "ec")) {
+				rsa = false;
+			} else {
+				fprintf(stderr, "error: invalid algorithm\n");
 				return 1;
 			}
-			ksize = (unsigned int)atoi(arg[1] + 4);
 			arg++;
 		} else if (!strcmp(*arg, "-days") && arg[1]) {
 			days = (unsigned int)atoi(arg[1]);
+			arg++;
+		} else if (!strcmp(*arg, "-pkeyopt") && arg[1]) {
+			if (strncmp(arg[1], "ec_paramgen_curve:", 18)) {
+				fprintf(stderr, "error: invalid pkey option: %s\n", arg[1]);
+				return 1;
+			}
+			curve = ecp_curve((const char *)(arg[1] + 18));
+			if (curve == MBEDTLS_ECP_DP_NONE) {
+				fprintf(stderr, "error: invalid curve name: %s\n", arg[1] + 18);
+				return 1;
+			}
 			arg++;
 		} else if (!strcmp(*arg, "-keyout") && arg[1]) {
 			keypath = arg[1];
@@ -195,8 +247,7 @@ int selfsigned(char **arg)
 		}
 		arg++;
 	}
-
-	gen_key(&key, ksize, exp, pem);
+	gen_key(&key, rsa, ksize, exp, curve, pem);
 
 	if (keypath)
 		write_key(&key, keypath, pem);
@@ -211,34 +262,34 @@ int selfsigned(char **arg)
 	fprintf(stderr, "Generating selfsigned certificate with subject '%s'"
 			" and validity %s-%s\n", subject, fstr, tstr);
 
-	x509write_crt_init(&cert);
-	x509write_crt_set_md_alg(&cert, POLARSSL_MD_SHA1);
-	x509write_crt_set_issuer_key(&cert, &key);
-	x509write_crt_set_subject_key(&cert, &key);
-	x509write_crt_set_subject_name(&cert, subject);
-	x509write_crt_set_issuer_name(&cert, subject);
-	x509write_crt_set_validity(&cert, fstr, tstr);
-	x509write_crt_set_basic_constraints(&cert, 0, -1);
-	x509write_crt_set_subject_key_identifier(&cert);
-	x509write_crt_set_authority_key_identifier(&cert);
+	mbedtls_x509write_crt_init(&cert);
+	mbedtls_x509write_crt_set_md_alg(&cert, MBEDTLS_MD_SHA256);
+	mbedtls_x509write_crt_set_issuer_key(&cert, &key);
+	mbedtls_x509write_crt_set_subject_key(&cert, &key);
+	mbedtls_x509write_crt_set_subject_name(&cert, subject);
+	mbedtls_x509write_crt_set_issuer_name(&cert, subject);
+	mbedtls_x509write_crt_set_validity(&cert, fstr, tstr);
+	mbedtls_x509write_crt_set_basic_constraints(&cert, 0, -1);
+	mbedtls_x509write_crt_set_subject_key_identifier(&cert);
+	mbedtls_x509write_crt_set_authority_key_identifier(&cert);
 
-	_urandom(NULL, buf, 8);
+	_urandom(NULL, (void *) buf, 8);
 	for (len = 0; len < 8; len++)
 		sprintf(sstr + len*2, "%02x", (unsigned char) buf[len]);
 
-	mpi_init(&serial);
-	mpi_read_string(&serial, 16, sstr);
-	x509write_crt_set_serial(&cert, &serial);
+	mbedtls_mpi_init(&serial);
+	mbedtls_mpi_read_string(&serial, 16, sstr);
+	mbedtls_x509write_crt_set_serial(&cert, &serial);
 
 	if (pem) {
-		if (x509write_crt_pem(&cert, (void *) buf, sizeof(buf), _urandom, NULL) < 0) {
+		if (mbedtls_x509write_crt_pem(&cert, (void *) buf, sizeof(buf), _urandom, NULL) < 0) {
 			fprintf(stderr, "Failed to generate certificate\n");
 			return 1;
 		}
 
 		len = strlen(buf);
 	} else {
-		len = x509write_crt_der(&cert, (void *) buf, sizeof(buf), _urandom, NULL);
+		len = mbedtls_x509write_crt_der(&cert, (void *) buf, sizeof(buf), _urandom, NULL);
 		if (len < 0) {
 			fprintf(stderr, "Failed to generate certificate: %d\n", len);
 			return 1;
@@ -246,9 +297,9 @@ int selfsigned(char **arg)
 	}
 	write_file(certpath, len, pem);
 
-	x509write_crt_free(&cert);
-	mpi_free(&serial);
-	pk_free(&key);
+	mbedtls_x509write_crt_free(&cert);
+	mbedtls_mpi_free(&serial);
+	mbedtls_pk_free(&key);
 
 	return 0;
 }
@@ -259,8 +310,10 @@ int main(int argc, char *argv[])
 
 	if (!argv[1]) {
 		//Usage
+	} else if (!strcmp(argv[1], "eckey")) {
+		return dokey(false, argv+2);
 	} else if (!strcmp(argv[1], "rsakey")) {
-		return rsakey(argv+2);
+		return dokey(true, argv+2);
 	} else if (!strcmp(argv[1], "selfsigned")) {
 		return selfsigned(argv+2);
 	}
@@ -268,6 +321,6 @@ int main(int argc, char *argv[])
 	fprintf(stderr,
 		"PX5G X.509 Certificate Generator Utility v" PX5G_VERSION "\n" PX5G_COPY
 		"\nbased on PolarSSL by Christophe Devine and Paul Bakker\n\n");
-	fprintf(stderr, "Usage: %s [rsakey|selfsigned]\n", *argv);
+	fprintf(stderr, "Usage: %s [eckey|rsakey|selfsigned]\n", *argv);
 	return 1;
 }
