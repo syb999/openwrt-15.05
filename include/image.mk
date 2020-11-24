ab3f7055f3550e15fa92251f49be5ffdf5287190
syb999 <13062678135@163.com>
2020-11-24 19:11:01 +0800

    revert rootfs.mk

--- include/image.mk
+++ include/image.mk
@@ -285,15 +285,16 @@ mkfs_cur_target_dir = $(call mkfs_target_dir,pkg=$(target_params))
 
 opkg_target = \
 	$(call opkg,$(mkfs_cur_target_dir)) \
-		-f $(mkfs_cur_target_dir).conf
+		-f $(mkfs_cur_target_dir).conf \
+		-l $(mkfs_cur_target_dir).tmp
 
 target-dir-%: FORCE
 	rm -rf $(mkfs_cur_target_dir) $(mkfs_cur_target_dir).opkg
-	$(CP) $(TARGET_DIR_ORIG) $(mkfs_cur_target_dir)
+	$(CP) $(TARGET_DIR) $(mkfs_cur_target_dir)
 	-mv $(mkfs_cur_target_dir)/etc/opkg $(mkfs_cur_target_dir).opkg
 	echo 'src default file://$(PACKAGE_DIR_ALL)' > $(mkfs_cur_target_dir).conf
 	$(if $(mkfs_packages_remove), \
-		-$(call opkg,$(mkfs_cur_target_dir)) remove \
+		$(call opkg,$(mkfs_cur_target_dir)) remove \
 			$(mkfs_packages_remove))
 	$(if $(call opkg_package_files,$(mkfs_packages_add)), \
 		$(opkg_target) update && \
@@ -301,7 +302,9 @@ target-dir-%: FORCE
 			$(call opkg_package_files,$(mkfs_packages_add)))
 	$(call prepare_rootfs,$(mkfs_cur_target_dir))
 	-mv $(mkfs_cur_target_dir).opkg $(mkfs_cur_target_dir)/etc/opkg
-	rm -f $(mkfs_cur_target_dir).conf
+	rm -rf \
+		$(mkfs_cur_target_dir).conf \
+		$(mkfs_cur_target_dir).tmp
 
 $(KDIR)/root.%: kernel_prepare
 	$(call Image/mkfs/$(word 1,$(target_params)),$(target_params))
--- include/rootfs.mk
+++ include/rootfs.mk
@@ -25,7 +25,7 @@ ifdef CONFIG_USE_MKLIBS
 	$(RSTRIP) $(TMP_DIR)/mklibs-out
 	for lib in `ls $(TMP_DIR)/mklibs-out/*.so.* 2>/dev/null`; do \
 		LIB="$${lib##*/}"; \
-		DEST="`ls "$(1)/lib/$$LIB" "$(1)/usr/lib/$$LIB" 2>/dev/null`"; \
+		DEST="`ls "$(TARGET_DIR)/lib/$$LIB" "$(TARGET_DIR)/usr/lib/$$LIB" 2>/dev/null`"; \
 		[ -n "$$DEST" ] || continue; \
 		echo "Copying stripped library $$lib to $$DEST"; \
 		cp "$$lib" "$$DEST" || exit 1; \
@@ -34,46 +34,44 @@ ifdef CONFIG_USE_MKLIBS
 endif
 
 # where to build (and put) .ipk packages
-opkg = \
+OPKG:= \
   IPKG_NO_SCRIPT=1 \
-  IPKG_INSTROOT=$(1) \
-  TMPDIR=$(1)/tmp \
-  $(STAGING_DIR_HOST)/bin/opkg \
-	--offline-root $(1) \
+  IPKG_TMP=$(TMP_DIR)/ipkg \
+  IPKG_INSTROOT=$(TARGET_DIR) \
+  IPKG_CONF_DIR=$(STAGING_DIR)/etc \
+  IPKG_OFFLINE_ROOT=$(TARGET_DIR) \
+  $(XARGS) $(STAGING_DIR_HOST)/bin/opkg \
+	--offline-root $(TARGET_DIR) \
+	--force-depends \
+	--force-overwrite \
 	--force-postinstall \
+	--force-maintainer \
 	--add-dest root:/ \
 	--add-arch all:100 \
 	--add-arch $(if $(ARCH_PACKAGES),$(ARCH_PACKAGES),$(BOARD)):200
 
-opkg_package_files = $(wildcard \
-	$(foreach dir,$(PACKAGE_SUBDIRS), \
-	  $(foreach pkg,$(1), $(dir)/$(pkg)_*.ipk)))
-
-TARGET_DIR_ORIG := $(TARGET_ROOTFS_DIR)/root.orig-$(BOARD)
-
 define prepare_rootfs
 	@if [ -d $(TOPDIR)/files ]; then \
-		$(call file_copy,$(TOPDIR)/files/.,$(1)); \
+		$(call file_copy,$(TOPDIR)/files/.,$(TARGET_DIR)); \
 	fi
-	@mkdir -p $(1)/etc/rc.d
+	@mkdir -p $(TARGET_DIR)/etc/rc.d
 	@( \
-		cd $(1); \
+		cd $(TARGET_DIR); \
 		for script in ./usr/lib/opkg/info/*.postinst; do \
-			IPKG_INSTROOT=$(1) $$(which bash) $$script; \
+			IPKG_INSTROOT=$(TARGET_DIR) $$(which bash) $$script; \
 		done; \
 		for script in ./etc/init.d/*; do \
 			grep '#!/bin/sh /etc/rc.common' $$script >/dev/null || continue; \
-			IPKG_INSTROOT=$(1) $$(which bash) ./etc/rc.common $$script enable; \
+			IPKG_INSTROOT=$(TARGET_DIR) $$(which bash) ./etc/rc.common $$script enable; \
 		done || true \
 	)
-	$(if $(SOURCE_DATE_EPOCH),sed -i "s/Installed-Time: .*/Installed-Time: $(SOURCE_DATE_EPOCH)/" $(1)/usr/lib/opkg/status)
-	@-find $(1) -name CVS   | $(XARGS) rm -rf
-	@-find $(1) -name .svn  | $(XARGS) rm -rf
-	@-find $(1) -name .git  | $(XARGS) rm -rf
-	@-find $(1) -name '.#*' | $(XARGS) rm -f
-	rm -f $(1)/usr/lib/opkg/lists/*
-	rm -f $(1)/usr/lib/opkg/info/*.postinst*
-	rm -f $(1)/usr/lib/opkg/info/*.prerm*
-	$(if $(CONFIG_CLEAN_IPKG),rm -rf $(1)/usr/lib/opkg)
-	$(call mklibs,$(1))
+	$(if $(SOURCE_DATE_EPOCH),sed -i "s/Installed-Time: .*/Installed-Time: $(SOURCE_DATE_EPOCH)/" $(TARGET_DIR)/usr/lib/opkg/status)
+	@-find $(TARGET_DIR) -name CVS   | $(XARGS) rm -rf
+	@-find $(TARGET_DIR) -name .svn  | $(XARGS) rm -rf
+	@-find $(TARGET_DIR) -name .git  | $(XARGS) rm -rf
+	@-find $(TARGET_DIR) -name '.#*' | $(XARGS) rm -f
+	rm -f $(TARGET_DIR)/usr/lib/opkg/info/*.postinst*
+	rm -f $(TARGET_DIR)/usr/lib/opkg/info/*.prerm*
+	$(if $(CONFIG_CLEAN_IPKG),rm -rf $(TARGET_DIR)/usr/lib/opkg)
+	$(mklibs)
 endef
