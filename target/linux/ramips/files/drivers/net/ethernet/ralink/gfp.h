13a2586ce03c4815a8334f8a4f7815bf2b0c2417
syb999 <13062678135@163.com>
2020-11-25 22:49:42 +0800

    ramips:ethernet:gfp.h: Update

--- target/linux/ramips/files/drivers/net/ethernet/ralink/gfp.h
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/gfp.h
@@ -11,12 +11,9 @@
 #endif
 
 struct page_frag_cache;
-extern void __page_frag_cache_drain(struct page *page,
-										unsigned int count);
-extern void *__alloc_page_frag(struct page_frag_cache *nc,
-			       unsigned int fragsz, gfp_t gfp_mask);
-extern void *page_frag_alloc(struct page_frag_cache *nc,
-			       unsigned int fragsz, gfp_t gfp_mask);
+extern void __page_frag_cache_drain(struct page *page, unsigned int count);
+extern void *__alloc_page_frag(struct page_frag_cache *nc, unsigned int fragsz, gfp_t gfp_mask);
+extern void *page_frag_alloc(struct page_frag_cache *nc, unsigned int fragsz, gfp_t gfp_mask);
 extern void __free_page_frag(void *addr);
 extern void page_frag_free(void *addr);
 
