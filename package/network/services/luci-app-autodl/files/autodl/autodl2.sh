ef0aafc3c1fdf73182dbf0d682565592cfa150dc
syb999 <13062678135@163.com>
2020-12-09 17:19:08 +0800

    luci-app-autodl: support www.xgys.net

---
+++ package/network/services/luci-app-autodl/files/autodl/autodl2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+autodlgeturl=$(cat /tmp/autodl.url)
+autodlgetpath=$(cat /tmp/autodl.path)
+
+
+if [ ! -d "/autodl/videos" ]; then
+  mkdir /autodl
+  chmod 777 /autodl
+  ln -s $autodlgetpath /autodl
+fi
+
+
+
+curl $autodlgeturl | grep m3u8 > /tmp/autodldmdm.1
+sleep 3
+aurl=$(cat /tmp/autodldmdm.1)
+echo ${aurl#*now=\"} > /tmp/autodldmdm.2
+a2url=$(cat /tmp/autodldmdm.2)
+echo ${a2url%\"\;var pn*} > /tmp/autodldmdm.3
+a3url=$(cat /tmp/autodldmdm.3)
+curl $a3url > /tmp/autodldmdm.4
+sleep 3
+a4url=$(tail -n 1 /tmp/autodldmdm.4)
+echo $a3url | sed 's/index.m3u8/1000k\/hls\/&/' > /tmp/autodldmdm.5
+a5url=$(echo $a3url | sed 's/index.m3u8/1000k\/hls\/&/')
+
+echo -en "$a5url\n" | python3 /usr/autodl/autodl.py
+
--- package/network/services/luci-app-autodl/luasrc/model/cbi/autodl.lua
+++ package/network/services/luci-app-autodl/luasrc/model/cbi/autodl.lua
@@ -1,34 +1,46 @@
 m = Map("autodl", translate("Autodl"))
 
-s = m:section(TypedSection, "autodl", "", translate("Assistant for automatic download m3u8 videos form https://www.dy10000.com."))
+s = m:section(TypedSection, "autodl", "", translate("Assistant for automatic download m3u8 videos."))
 s.anonymous = true
 s.addremove = false
 
-s:tab("autodl", translate("Basic"))
+s:tab("basic", translate("Basic Setting"))
 
-url=s:taboption("autodl", Value, "url", translate("Video URL"))
+url=s:taboption("basic", Value, "url", translate("Video URL"))
 url.rmempty = true
 url.datatype = "string"
 url.description = translate("下载视频的网页地址")
 
-path=s:taboption("autodl", Value, "path", translate("Directory"))
+path=s:taboption("basic", Value, "path", translate("Directory"))
 path.datatype = "string"
 path.default = "/mnt/sda3/videos"
 path.rmempty = false
 path.description = translate("保存视频的地址")
 
-url1=s:taboption("autodl", Button, "url1", translate("同步下载地址(同步后先保存&应用)"))
+url1=s:taboption("basic", Button, "url1", translate("同步下载地址(同步后先保存&应用)"))
 url1.inputstyle = "apply"
 
 
-au = s:taboption("autodl", Button, "_autodl", translate("download"))
+s:tab("autodl1", translate("Download form https://www.dy10000.com"))
+au = s:taboption("autodl1", Button, "_autodl1", translate("download"))
 au.inputstyle = "apply"
 function au.write(self, section)
     luci.util.exec("uci get autodl.@autodl[0].url > /tmp/autodl.url")
     luci.util.exec("sleep 1")
     luci.util.exec("uci get autodl.@autodl[0].path > /tmp/autodl.path")
     luci.util.exec("sleep 2")
-    luci.util.exec("/usr/autodl/autodl.sh &")
+    luci.util.exec("/usr/autodl/autodl1.sh &")
+end
+
+s:tab("autodl2", translate("Download form https://www.xgys.net"))
+au = s:taboption("autodl2", Button, "_autodl2", translate("download"))
+au.inputstyle = "apply"
+function au.write(self, section)
+    luci.util.exec("uci get autodl.@autodl[0].url > /tmp/autodl.url")
+    luci.util.exec("sleep 1")
+    luci.util.exec("uci get autodl.@autodl[0].path > /tmp/autodl.path")
+    luci.util.exec("sleep 2")
+    luci.util.exec("/usr/autodl/autodl2.sh &")
 end
 
 return m
