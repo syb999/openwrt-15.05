2d68965119a6e3201d05a8d0852c3daece7cc6d0
syb999 <13062678135@163.com>
2021-01-20 00:36:44 +0800

    luci-app-autodl: fix the autodlxmly.sh script

--- package/network/services/luci-app-autodl/files/autodl/autodlxmly.sh
+++ package/network/services/luci-app-autodl/files/autodl/autodlxmly.sh
@@ -49,7 +49,22 @@ do
 	adurlprefix=$urlprefix
 	adurlsuffix=$urlsuffix
 	tmpgetaudiourl="${adurlprefix}${xmlytrackId}${adurlsuffix}"
-	curl -s --retry 3 --retry-delay 2 --connect-timeout 10 -m 20 -v $tmpgetaudiourl > /tmp/tmpXM.xmlyhttp6
+	curl -s --retry 3 --retry-delay 2 --connect-timeout 10 -m 20 http://www.ximalaya.com/revision/time > /tmp/tmp.XM.xmtimestamp
+	xmtimestamp=$(cat /tmp/tmp.XM.xmtimestamp)
+	xmvvmd5=$(echo himalaya-$xmtimestamp)
+	echo -n $xmvvmd5 | md5sum > /tmp/tmp.XM.md5s
+	cat /tmp/tmp.XM.md5s | cut -d ' ' -f 1 > /tmp/tmp.XM.md5ss
+	xmsign1=$(cat /tmp/tmp.XM.md5ss)
+	head -n 128 /dev/urandom | tr -dc "123456789" | head -c2 > /tmp/tmp.XM.randum1
+	rnum1=\($(cat /tmp/tmp.XM.randum1)\)
+	head -n 128 /dev/urandom | tr -dc "123456789" | head -c2 > /tmp/tmp.XM.randum2
+	rnum2=\($(cat /tmp/tmp.XM.randum2)\)
+	head -n 128 /dev/urandom | tr -dc "012345" | head -c3 > /tmp/tmp.XM.randum3
+	bnum=$(cat /tmp/tmp.XM.randum3)
+	date +"%s" > /tmp/tmp.XM.datelst
+	optimestamp=$(cat /tmp/tmp.XM.datelst)
+	cxmsign=${xmsign1}${rnum1}${xmtimestamp}${rnum2}${optimestamp}${bnum}
+	curl -s --retry 3 --retry-delay 2 --connect-timeout 10 -m 20 -H ""xm-sign": "$cxmsign"" -v $tmpgetaudiourl > /tmp/tmpXM.xmlyhttp6
 	sleep 1
 	tmpaudio=$(cat /tmp/tmpXM.xmlyhttp6)
 	echo ${tmpaudio#*src\":\"} > /tmp/tmpXM.xmlyhttp7
@@ -59,6 +74,8 @@ do
 	wget-ssl -q -c $audiofile -O $paudionum.m4a
 	sleep 3
 	paudionum=$(echo `expr $paudionum - 1`)
+	rm /tmp/tmp.XM.md*
+	rm /tmp/tmp.XM.xmtimestamp
 done
 
 cat /tmp/tmpXM.xmlyhttp3 | grep trackId > /tmp/tmpXM.xmlyhttp0num
