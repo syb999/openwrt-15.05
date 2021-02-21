66a80ea36f5e26a3e211c8737a09c3ecfb104ce9
syb999 <13062678135@163.com>
2021-02-21 17:04:19 +0800

    add mt7615-dbdc-setup package
    support config mt7615-dbdc on Luci wireless page

---
+++ package/kernel/mt7615-dbdc-setup/files/wifi/wifi-init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+wifi_setup_radio()
+{
+	if test -e /sys/kernel/debug/ieee80211/phy0/mt76/dbdc &&
+	   [ "$(readlink /sys/class/ieee80211/phy0/device)" = "$(readlink /sys/class/ieee80211/phy1/device)" ]; then
+		cp /lib/wifi/mt7615dbdc.conf /etc/config/wireless
+		getphy0mac=$(cat /sys/class/ieee80211/phy0/macaddress)
+		getphy1mac=$(cat /sys/class/ieee80211/phy1/macaddress)
+		uci set wireless.radio0.macaddr=$getphy0mac
+		uci set wireless.radio1.macaddr=$getphy1mac
+		sed -i 's/START=60/START=70/' /etc/init.d/wifistart
+	fi
+}
+
+wifi_first_init()
+{
+	wifi_setup_radio
+	uci commit wireless
+}
