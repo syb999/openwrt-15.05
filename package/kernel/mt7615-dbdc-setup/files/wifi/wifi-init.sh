0c089010049c8e4e356b82e243c76acabee6145c
syb999 <13062678135@163.com>
2021-06-12 02:22:59 +0800

    mt7615-dbdc-setup: update

--- package/kernel/mt7615-dbdc-setup/files/wifi/wifi-init.sh
+++ package/kernel/mt7615-dbdc-setup/files/wifi/wifi-init.sh
@@ -5,10 +5,6 @@ wifi_setup_radio()
 	if test -e /sys/kernel/debug/ieee80211/phy0/mt76/dbdc &&
 	   [ "$(readlink /sys/class/ieee80211/phy0/device)" = "$(readlink /sys/class/ieee80211/phy1/device)" ]; then
 		cp /lib/wifi/mt7615dbdc.conf /etc/config/wireless
-		getphy0mac=$(cat /sys/class/ieee80211/phy0/macaddress)
-		getphy1mac=$(cat /sys/class/ieee80211/phy1/macaddress)
-		uci set wireless.radio0.macaddr=$getphy0mac
-		uci set wireless.radio1.macaddr=$getphy1mac
 		sed -i 's/START=60/START=70/' /etc/init.d/wifistart
 		sed -i 's/\/sbin\/wifi/sleep\ 10\n\t\/sbin\/wifi/' /etc/init.d/wifistart
 	fi
