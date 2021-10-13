0b45dac8a2eedf7ecc8d37e327a1a3e9b1dda631
syb999 <13062678135@163.com>
2021-10-13 21:40:25 +0800

    ramips: update makefile

--- target/linux/ramips/mt7621-nand/target.mk
+++ target/linux/ramips/mt7621-nand/target.mk
@@ -5,7 +5,7 @@
 SUBTARGET:=mt7621-nand
 BOARDNAME:=MT7621 NAND flash based boards
 ARCH_PACKAGES:=ramips_1004kc
-FEATURES+=usb squashfs nand ubifs ramdisk
+FEATURES+=usb nand ubifs ramdisk
 CPU_TYPE:=1004kc
 CPU_SUBTYPE:=dsp
 CFLAGS:=-Os -pipe -mmt -mips32r2 -mtune=1004kc
