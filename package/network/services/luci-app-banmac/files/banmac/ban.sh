bf735e18f7e78ca534c85bb3a50f9d9b274485f9
syb999 <13062678135@163.com>
2021-11-15 22:46:18 +0800

    luci-app-banmac: remove the wireless client at the ban time

--- package/network/services/luci-app-banmac/files/banmac/ban.sh
+++ package/network/services/luci-app-banmac/files/banmac/ban.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-getcmac=$(uci get banmac.@banlist[0].banlist_mac)
-
+getcmac=$(uci get banmac.@banlist[0].banlist_mac | tr 'A-Z' 'a-z')
 iptables -I FORWARD -m mac --mac-source $getcmac -j DROP
 
+for i in $(seq 0 1)
+do
+	for x in $(iw wlan${i} station dump | grep -i station | cut -d ' ' -f 2)
+	do
+		if [ $x = $getcmac ]; then
+			iw dev wlan${i} station del $x
+		fi
+	done
+done
