7983341bd96dddcdf452c1f9cddd1e03a1b139b1
syb999 <13062678135@163.com>
2021-11-18 17:20:33 +0800

    luci-app-banmac: fix log delay

--- package/network/services/luci-app-banmac/files/banmac/ban.sh
+++ package/network/services/luci-app-banmac/files/banmac/ban.sh
@@ -3,6 +3,9 @@
 sectionname=$(echo $0 | cut -d '_' -f 2 | sed 's/^OO!%!OO//')
 getcmac=$(uci get banmac.$sectionname.banlist_mac | tr 'A-Z' 'a-z')
 iptables -I FORWARD -m mac --mac-source $getcmac -j DROP
+hostname=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 4)
+hostip=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 3)
+echo "★禁网设备：$hostname($hostip) MAC地址：$getcmac 操作日期：$(date +%Y年%m月%d日\ %H点%M分%S秒)" >> /etc/banmaclog
 
 for i in $(seq 0 1)
 do
@@ -13,7 +16,3 @@ do
 		fi
 	done
 done
-
-hostname=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 4)
-hostip=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 3)
-echo "★禁网设备：$hostname($hostip) MAC地址：$getcmac 操作日期：$(date +%Y年%m月%d日\ %H点%M分%S秒)" >> /etc/banmaclog
