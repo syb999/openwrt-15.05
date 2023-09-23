ea5d6675325efffd5676331a07cc8962f7114cba
syb999 <13062678135@163.com>
2023-09-23 10:45:50 +0800

    panel-ap-setup: add second ssid

--- package/utils/panel-ap-setup/files/panel_ap/panel_ap.sh
+++ package/utils/panel-ap-setup/files/panel_ap/panel_ap.sh
@@ -11,7 +11,14 @@ panel_ap_first_init()
 	uci set wireless.radio0.txpower='13'
 	uci set wireless.@wifi-iface[0].ssid='OP_panel_ap'
 	uci set wireless.@wifi-iface[0].network='wan'
+	uci add /etc/config/wireless wifi-iface
+	uci set wireless.@wifi-iface[1].device='radio0'
+	uci set wireless.@wifi-iface[1].mode='ap'
+	uci set wireless.@wifi-iface[1].ssid='OP_panel_ap_local'
+	uci set wireless.@wifi-iface[1].network='lan'
+	uci set wireless.@wifi-iface[1].encryption='none'
 	uci commit wireless
 	sleep 3
 	/sbin/wifi
 }
+
