15767c96d6cc4cb824953716a7bbb2c09cb23800
syb999 <13062678135@163.com>
2024-02-29 12:07:52 +0800

    luci-app-autodl: xmlyfree.py: use pycryptodome instead of cryptography

--- package/network/services/luci-app-autodl/files/autodl/xmlyfree.py
+++ package/network/services/luci-app-autodl/files/autodl/xmlyfree.py
@@ -1,19 +1,17 @@
 import sys
 import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 import base64
 import requests
 import time
 
 def decrypt_url(ciphertext):
     key = bytes.fromhex("aaad3e4fd540b0f79dca95606e72bf93")
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+    cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = base64.urlsafe_b64decode(ciphertext + '=' * (4 - len(ciphertext) % 4))
-    decryptor = cipher.decryptor()
-    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-    padding_length = plaintext[-1]
-    plaintext = plaintext[:-padding_length]
+    plaintext = cipher.decrypt(ciphertext)
+    plaintext = unpad(plaintext, AES.block_size)
     plaintext = plaintext.decode("utf-8").encode("ascii", errors="ignore").decode()
     return plaintext
 
@@ -30,7 +28,7 @@ def analyze_sound(sound_id, headers):
     except Exception as e:
         print(f'ID为{sound_id}的声音解析失败！')
         return False
-    
+
     sound_info = ""
     for encrypted_url in encrypted_url_list:
         if encrypted_url["type"] == "MP3_64":
@@ -42,15 +40,14 @@ if __name__ == "__main__":
     headers = {
         "user-agent": "Mozilla/5.0"
     }
-     
+
     with open('/tmp/tmpXM.xmlysoundid', 'r') as f:
         sound_id = f.read().strip()
-        
+
     sound_info = analyze_sound(sound_id, headers)
-    
+
     if sound_info:
         output_file = os.path.join(os.path.dirname(__file__), '/tmp/tmpXM.xmlyhttp')
         with open(output_file, 'w', encoding='utf-8') as f:
             f.write(sound_info)
-
 
