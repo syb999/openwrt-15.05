3e3481fff965970c1f067f1a27fbf9b6b43118ab
syb999 <13062678135@163.com>
2025-04-19 12:31:29 +0800

    luci-app-autodl: remove expired modules

--- package/network/services/luci-app-autodl/luasrc/model/cbi/autodl.lua
+++ package/network/services/luci-app-autodl/luasrc/model/cbi/autodl.lua
@@ -40,145 +40,6 @@ endnum.rmempty = false
 endnum.description = translate("Please enter a valid number")
 
 
-s:tab("basic2", translate("Basic Setting for Audio"))
-
-xmlyurl=s:taboption("basic2", Value, "xmlyurl", translate("Audios URL"))
-xmlyurl.rmempty = true
-xmlyurl.datatype = "string"
-xmlyurl.description = translate("URL for downloading https://www.ximalaya.com Audios")
-
-xmlypagenum=s:taboption("basic2", Value, "xmlypagenum", translate("The starting page number"))
-xmlypagenum.rmempty = true
-xmlypagenum.datatype = "uinteger"
-xmlypagenum.default = "1"
-
-xmlymultiurl=s:taboption("basic2", Flag, "xmly_multi_pages", translate("Audios URL multi pages"))
-xmlymultiurl.description = translate("Need to download multiple pages of resources")
-
-xmlygetpages=s:taboption("basic2", Value, "xmlygetpages", translate("Number of pages to download"))
-xmlygetpages:depends("xmly_multi_pages", "1")
-xmlygetpages.datatype = "uinteger"
-xmlygetpages.default = "0"
-
-xmlyname=s:taboption("basic2", Value, "xmlyname", translate("Audios Name"))
-xmlyname.datatype = "string"
-xmlyname.placeholder = "story"
-xmlyname.default = "story"
-xmlyname.rmempty = false
-xmlyname.description = translate("Audios from https://www.ximalaya.com")
-
-xmlycvip = s:taboption("basic2", Flag, "wanna_get_VIP_audios", translate("Wanna get VIP audios"))
-xmlycvip.description = translate("You must have VIP accout")
-
-xmlycookie=s:taboption("basic2", Value, "xmlycookie", translate("All the values associated with a cookie"))
-xmlycookie:depends("wanna_get_VIP_audios", "1")
-xmlycookie.datatype = "string"
-xmlycookie.default = ""
-xmlycookie.description = translate("Using The Cookie of VIP account to get VIP audios")
-
-xmlysleeptime=s:taboption("basic2", Value, "xmlysleeptime", translate("Set delay time"))
-xmlysleeptime:depends("wanna_get_VIP_audios", "1")
-xmlysleeptime.datatype = "uinteger"
-xmlysleeptime.default = "0"
-
-xmlypath=s:taboption("basic2", Value, "xmlypath", translate("Download Audios directory"))
-xmlypath.datatype = "string"
-xmlypath.default = "/mnt/sda3/audios"
-xmlypath.rmempty = false
-xmlypath.description = translate("Please enter a valid directory")
-
-xmlygetlist = s:taboption("basic2", Button, "xmlygetlist", translate("Get list"))
-xmlygetlist.inputstyle = "apply"
-xmlygetlist.description = translate("Show current audio list")
-function xmlygetlist.write(self, section)
-    luci.util.exec("/usr/autodl/xmlygetlist.sh >/dev/null 2>&1 &")
-end
-
-xmlyopennum = s:taboption("basic2", Flag, "select_audio_numbers", translate("Select Audio number"))
-xmlyopennum.description = translate("Get list first")
-
-xmlysenum = s:taboption("basic2", ListValue, "audio_num", translate("Select Audio"))
-xmlysenum:depends("select_audio_numbers", "1")
-xmlysenum.default = "null"
-
-local xmlyplaynumf = { }
-local xmlyfload = io.open("/tmp/tmp.Audioxm.list", "r")
-
-if xmlyfload then
-	local listnumb
-	repeat
-		listnumb = xmlyfload:read("*l")
-		local s = listnumb
-		if s then xmlyplaynumf[#xmlyplaynumf+1] = s end
-	until not listnumb
-	xmlyfload:close()
-end
-
-for _, v in luci.util.vspairs(xmlyplaynumf) do
-    xmlysenum:value(v)
-end
-
-isydl = s:taboption("basic2", Flag, "wanna_get_ishuyin_audios", translate("Wanna get www.ishuyin.com audios"))
-
-isyurl=s:taboption("basic2", Value, "isyurl", translate("Audios URL"))
-isyurl:depends("wanna_get_ishuyin_audios", "1")
-isyurl.rmempty = true
-isyurl.datatype = "string"
-isyurl.description = translate("URL for downloading https://www.ishuyin.com Audios")
-
-isyname=s:taboption("basic2", Value, "isyname", translate("Audios Name"))
-isyname:depends("wanna_get_ishuyin_audios", "1")
-isyname.datatype = "string"
-isyname.placeholder = "story"
-isyname.default = "story"
-isyname.rmempty = true
-isyname.description = translate("Audios from https://www.ishuyin.com")
-
-isypath=s:taboption("basic2", Value, "isypath", translate("Download Audios directory"))
-isypath:depends("wanna_get_ishuyin_audios", "1")
-isypath.datatype = "string"
-isypath.default = "/mnt/sda3/audios"
-isypath.rmempty = true
-isypath.description = translate("Please enter a valid directory")
-
-isynumber=s:taboption("basic2", Value, "isynumber", translate("Download Audios numbers"))
-isynumber:depends("wanna_get_ishuyin_audios", "1")
-isynumber.datatype = "uinteger"
-isynumber.default = "1"
-isynumber.rmempty = true
-isynumber.description = translate("Please enter the total number")
-
-kugoudl = s:taboption("basic2", Flag, "wanna_get_kugou_audios", translate("Wanna get https://www.kugou.com/ts/ audios"))
-
-kugouurl=s:taboption("basic2", Value, "kugouurl", translate("Audios URL"))
-kugouurl:depends("wanna_get_kugou_audios", "1")
-kugouurl.rmempty = true
-kugouurl.datatype = "string"
-kugouurl.default = "https://www.kugou.com/ts/xiaoshuo/x76gwaa/"
-kugouurl.description = translate("URL for downloading https://www.kugou.com/ts/ Audios")
-
-kugouname=s:taboption("basic2", Value, "kugouname", translate("Audios Name"))
-kugouname:depends("wanna_get_kugou_audios", "1")
-kugouname.datatype = "string"
-kugouname.placeholder = "story"
-kugouname.default = "宇宙职业选手"
-kugouname.rmempty = true
-
-kugoupath=s:taboption("basic2", Value, "kugoupath", translate("Download Audios directory"))
-kugoupath:depends("wanna_get_kugou_audios", "1")
-kugoupath.datatype = "string"
-kugoupath.default = "/mnt/sda3/audios"
-kugoupath.rmempty = true
-kugoupath.description = translate("Please enter a valid directory")
-
-kugounumber=s:taboption("basic2", Value, "kugounumber", translate("Download Audios numbers"))
-kugounumber:depends("wanna_get_kugou_audios", "1")
-kugounumber.datatype = "uinteger"
-kugounumber.default = "10"
-kugounumber.rmempty = true
-kugounumber.description = translate("Please enter the total number")
-
-
 s:tab("basic3", translate("Basic Setting for docin"))
 
 docinurl=s:taboption("basic3", Value, "docinurl", translate("docin.com doc URL"))
@@ -214,45 +75,6 @@ au1.description = translate("Download from https://www.qdm88.com")
 function au1.write(self, section)
     luci.util.exec("/usr/autodl/qdm88.sh >/dev/null 2>&1 &")
 end
-
-s:tab("audioxmly", translate("Download Audio from https://www.ximalaya.com"))
-au3 = s:taboption("audioxmly", Button, "_audioxmly", translate("One-click download"))
-au3.inputstyle = "apply"
-au3.description = translate("Audios download")
-function au3.write(self, section)
-    luci.util.exec("/usr/autodl/autodlxmly.sh >/dev/null 2>&1 &")
-end
-
-au3v = s:taboption("audioxmly", Button, "_audioau3v", translate("One-click download freeVIP"))
-au3v.inputstyle = "apply"
-au3v.description = translate("node needs to be installed")
-function au3v.write(self, section)
-    luci.util.exec("/usr/autodl/autodlxmlyVIP.sh >/dev/null 2>&1 &")
-end
-
-au3t = s:taboption("audioxmly", Button, "_autodl3t", translate("One-click m4a to mp3"))
-au3t.inputstyle = "apply"
-au3t.description = translate("ffmpeg needs to be installed")
-function au3t.write(self, section)
-    luci.util.exec("/usr/autodl/m4atomp3.sh >/dev/null 2>&1 &")
-end
-
-au3isy = s:taboption("audioxmly", Button, "_audioisy", translate("www.ishuyin.com One-click download"))
-au3isy:depends("wanna_get_ishuyin_audios", "1")
-au3isy.inputstyle = "apply"
-au3isy.description = translate("Audios download from https://www.ishuyin.com")
-function au3isy.write(self, section)
-    luci.util.exec("/usr/autodl/autodlisy.sh >/dev/null 2>&1 &")
-end
-
-au3kugou = s:taboption("audioxmly", Button, "_audiokugou", translate("https://www.kugou.com/ts/ One-click download"))
-au3kugou:depends("wanna_get_kugou_audios", "1")
-au3kugou.inputstyle = "apply"
-au3kugou.description = translate("Audios download from https://www.kugou.com/ts/")
-function au3kugou.write(self, section)
-    luci.util.exec("/usr/autodl/autodlkugou.sh >/dev/null 2>&1 &")
-end
-
 
 s:tab("online_server", translate("在线解码服务"))
 ollist=s:taboption("online_server", ListValue, "ollist", translate("在线解码服务"))
@@ -290,14 +112,6 @@ olp2.placeholder = "80"
 olp2.default = "80"
 olp2.description = translate("请输入web服务端口号")
 
-oldlvip = s:taboption("online_server", Button, "_oldlvip", translate("用解码服务器下载新品限免VIP资源(本机免安装Node)"))
-oldlvip:depends("ollist", "olremote")
-oldlvip.inputstyle = "apply"
-oldlvip.description = translate("需要特定的解码服务器支持,本机需安装urlencode")
-function oldlvip.write(self, section)
-    luci.util.exec("/usr/autodl/ols/xmlyonline.sh >/dev/null 2>&1 &")
-end
-
 olm4amp3 = s:taboption("online_server", Button, "_olm4amp3", translate("m4a在线转码mp3"))
 olm4amp3:depends("ollist", "olremote")
 olm4amp3.inputstyle = "apply"
@@ -331,57 +145,8 @@ function qrcodemake.write(self, section)
     luci.util.exec("/usr/autodl/ols/onlineqrcode.sh >/dev/null 2>&1 &")
 end
 
-s:tab("online_serverp", translate("在线解码播放"))
-olalbumid = s:taboption("online_serverp", Value, "olalbumid", translate("Audios URL"))
-olalbumid:depends("ollist", "olremote")
-olalbumid.rmempty = true
-olalbumid.datatype = "string"
-olalbumid.description = translate("播放https://www.ximalaya.com音频的网址url")
-
-olpagenums = s:taboption("online_serverp", Value, "olpagenums", translate("起始集数"))
-olpagenums:depends("ollist", "olremote")
-olpagenums.datatype = "string"
-olpagenums.placeholder = "1"
-olpagenums.default = "1"
-olpagenums.description = translate("免费节目集数排序取反，最新一集定为第1集")
-
-olpagenume = s:taboption("online_serverp", Value, "olpagenume", translate("结束页码"))
-olpagenume:depends("ollist", "olremote")
-olpagenume.datatype = "string"
-olpagenume.placeholder = "2"
-olpagenume.default = "2"
-olpagenume.description = translate("每页30集")
-
-olplayfr = s:taboption("online_serverp", Button, "_olplayfr", translate("直接在线播放喜马拉雅免费节目"))
-olplayfr:depends("ollist", "olremote")
-olplayfr.inputstyle = "apply"
-function olplayfr.write(self, section)
-    luci.util.exec("/usr/autodl/ols/onlineplayxmf.sh >/dev/null 2>&1 &")
-end
-
-olplayvp = s:taboption("online_serverp", Button, "_olplayvp", translate("直接在线播放喜马拉雅新品限免VIP节目"))
-olplayvp:depends("ollist", "olremote")
-olplayvp.inputstyle = "apply"
-function olplayvp.write(self, section)
-    luci.util.exec("/usr/autodl/ols/onlineplayxmv.sh >/dev/null 2>&1 &")
-end
-
-
-olnext = s:taboption("online_serverp", Button, "_olnext", translate("播放下一集"))
-olnext:depends("ollist", "olremote")
-olnext.inputstyle = "apply"
-function olnext.write(self, section)
-    luci.util.exec("/usr/autodl/ols/onlinenext.sh >/dev/null 2>&1 &")
-end
-
-olstop = s:taboption("online_serverp", Button, "_olstop", translate("停止播放"))
-olstop:depends("ollist", "olremote")
-olstop.inputstyle = "apply"
-function olstop.write(self, section)
-    luci.util.exec("/usr/autodl/ols/onlinestop.sh >/dev/null 2>&1 &")
-end
-
 s:tab("autodldocin", translate("Download from https://www.docin.com"))
+
 au4 = s:taboption("autodldocin", Button, "_autodldocin", translate("One-click download documents"))
 au4.inputstyle = "apply"
 au4.description = translate("docin.com documents download")
@@ -409,39 +174,6 @@ hellogst = s:taboption("audioplaytab", Button, "hellogst", translate("One-click 
 hellogst.inputstyle = "apply"
 function hellogst.write(self, section)
     luci.util.exec("sed -i 's/gstplaybroken/gst-play-1.0/' /usr/autodl/testplayer >/dev/null 2>&1 &")
-end
-
-au3selectedplay = s:taboption("audioplaytab", Button, "_autodl3selectedplay", translate("One-click Play selected mp3(m4a aac)"))
-au3selectedplay.inputstyle = "apply"
-au3selectedplay.description = translate("USB sound card is needed and gst-play-1.0 or mpg123 package has been installed")
-function au3selectedplay.write(self, section)
-    luci.util.exec("/usr/autodl/playselectedmp3a.sh >/dev/null 2>&1 &")
-end
-
-au3play = s:taboption("audioplaytab", Button, "_autodl3play", translate("One-click Play mp3(Positive)"))
-au3play.inputstyle = "apply"
-au3play.description = translate("USB sound card is needed and gst-play-1.0 or mpg123 package has been installed")
-function au3play.write(self, section)
-    luci.util.exec("/usr/autodl/playmp3a.sh >/dev/null 2>&1 &")
-end
-
-au3stop = s:taboption("audioplaytab", Button, "_autodl3stop", translate("Stop play mp3"))
-au3stop.inputstyle = "apply"
-function au3stop.write(self, section)
-    luci.util.exec("/usr/autodl/stopmp3.sh >/dev/null 2>&1 &")
-end
-
-au3playlastest = s:taboption("audioplaytab", Button, "_autodl3playlastest", translate("One-click Play mp3(Reverse)"))
-au3playlastest.inputstyle = "apply"
-au3playlastest.description = translate("USB sound card is needed and gst-play-1.0 or mpg123 package has been installed")
-function au3playlastest.write(self, section)
-    luci.util.exec("/usr/autodl/playlastestmp3a.sh >/dev/null 2>&1 &")
-end
-
-au3next = s:taboption("audioplaytab", Button, "_autodl3next", translate("Play Next mp3"))
-au3next.inputstyle = "apply"
-function au3next.write(self, section)
-    luci.util.exec("/usr/autodl/playnext.sh >/dev/null 2>&1 &")
 end
 
 au3vup = s:taboption("audioplaytab", Button, "_autodl3vup", translate("Volume Up"))
