c9d1e27911c1d16cccb8156368fb9f5267232454
syb999 <1306267135@163.com>
2025-05-25 08:40:25 +0800

    prereq-build: add Python 3.12 support

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -133,6 +133,7 @@ $(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
 $(eval $(call CleanupPython2))
 
 $(eval $(call SetupHostCommand,python,Please install Python >= 3.6, \
+	python3.12 -V 2>&1 | grep 'Python 3', \
 	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
@@ -143,6 +144,7 @@ $(eval $(call SetupHostCommand,python,Please install Python >= 3.6, \
 	python3 -V 2>&1 | grep -E 'Python 3\.[5-9]|[0-9][0-9]\.?'))
 
 $(eval $(call SetupHostCommand,python3,Please install Python >= 3.6, \
+	python3.12 -V 2>&1 | grep 'Python 3', \
 	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
