42644496c6db1ee33cac39c8cfee2d052d72467e
syb999 <13062678135@163.com>
2025-06-22 15:12:35 +0800

    luci-app-autodl: qdm88.sh: update

--- package/network/services/luci-app-autodl/files/autodl/qdm88.sh
+++ package/network/services/luci-app-autodl/files/autodl/qdm88.sh
@@ -37,12 +37,37 @@ for i in $(seq ${start_num} ${end_num});do
 		num_v=$i
 	fi
 
+	p_prefix="$(echo ${decrypto_url} | awk -F'/' '{OFS="/"; $NF=""; sub("/$",""); print}')"
+
 	wget-ssl -q $(uci get network.lan.ipaddr) -O /tmp/tmp.autodl.testwget > /dev/null 2>&1
 	if [ -s /tmp/tmp.autodl.testwget ];then
-		wget-ssl --timeout=25 -q "${decrypto_url}" -O "$autodlgetpath/$autodlgetname/第${num_v}集.mp4"
+		WGET=$(which wget-ssl)
 	else
-		wget --timeout=25 -q "${decrypto_url}" -O "$autodlgetpath/$autodlgetname/第${num_v}集.mp4"
+		WGET=$(which wget)
 	fi
 
+	$WGET --timeout=25 -q "${decrypto_url}" -O "$autodlgetpath/$autodlgetname/tmp.autodl.xm3u8"
+
+	if [ -z "$(file $autodlgetpath/$autodlgetname/tmp.autodl.xm3u8 | grep MP4)" ];then
+		p_suffix="$(cat /tmp/tmp.autodl.xm3u8 | grep m3u8)"
+
+		p_middle="$(echo ${p_suffix} | awk -F'/' '{OFS="/"; $NF=""; print}')"
+		new_url="${p_prefix}/${p_suffix}"
+		$WGET --timeout=25 -q "${new_url}" -O "$autodlgetpath/$autodlgetname/tmp.autodl.xm3u8"
+
+		for i in $(cat "$autodlgetpath/$autodlgetname/tmp.autodl.xm3u8" | grep ts);do
+			ts_url="${p_prefix}/${p_middle}${i}"
+			$WGET --timeout=25 -q "${ts_url}" -O "/tmp/tmp.autodl.fragts"
+			cat /tmp/tmp.autodl.fragts >> "$autodlgetpath/$autodlgetname/第${num_v}集.ts"
+		done
+
+		FFMPEG=$(which ffmpeg)
+		$FFMPEG -i "$autodlgetpath/$autodlgetname/第${num_v}集.ts" "$autodlgetpath/$autodlgetname/第${num_v}集.mp4"
+
+		rm "$autodlgetpath/$autodlgetname/tmp.autodl.xm3u8"
+		rm /tmp/tmp.autodl.*
+	else
+		mv "$autodlgetpath/$autodlgetname/tmp.autodl.xm3u8" "$autodlgetpath/$autodlgetname/第${num_v}集.mp4"
+	fi
 done
 
